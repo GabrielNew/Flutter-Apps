f60fce678cca704b18ce4049f787291382bb5ead
Gabriel wolf <gabrielfai70@gmail.com>
2020-07-27 22:22:45 -0300

    Campos da Tela de Contato

--- lista_de_contatos/lib/ui/contact_page.dart
+++ lista_de_contatos/lib/ui/contact_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:lista_de_contatos/helpers/contact_helper.dart';
 
@@ -13,6 +15,11 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
 
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  bool _userEdited = false;
   Contact _editedContact;
 
   @override
@@ -23,6 +30,10 @@ class _ContactPageState extends State<ContactPage> {
       _editedContact = Contact();
     } else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      nameController.text = _editedContact.name;
+      emailController.text = _editedContact.email;
+      phoneController.text = _editedContact.phone;
     }
   }
 
@@ -40,6 +51,53 @@ class _ContactPageState extends State<ContactPage> {
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
         ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140.0,
+                height: 140.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: _editedContact.img != null ? FileImage(File(_editedContact.img)): AssetImage("assets/person.png"),
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text){
+                _userEdited = true;
+                setState(() {
+                  _editedContact.name = text;
+                });
+              },
+            ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: "Email"),
+              onChanged: (text){
+                _userEdited = true;
+                _editedContact.email = text;
+              },  
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: phoneController,
+              decoration: InputDecoration(labelText: "Telefone"),
+              onChanged: (text){
+                _userEdited = true;
+                _editedContact.phone = text;
+              },
+              keyboardType: TextInputType.phone,  
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
