000e6cf0f6d2b958399c4642401f07fe4574877a
Gabriel wolf <gabrielfai70@gmail.com>
2020-08-05 20:18:54 -0300

    Início do app

---
+++ chat/lib/main.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Chat",
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
